adc0b58680530be11be3d9e957f8e1d54ace1cff
cem <mesutcemakin@gmail.com>
2025-04-28 18:16:00 +0300

    A certificate exchange mechanism implemented.

--- client.c
+++ client.c
@@ -8,62 +8,44 @@
 #include <netdb.h>
 #include <openssl/aes.h>
 #include <openssl/rand.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <libgen.h> 
 
 #define SERVER_ADRESS	"127.0.0.1"
 #define SERVER_PORT		55555
 #define BUFFER_SIZE		4096
+#define SVR_KEY_PATH    "./ClientFs/sample_public.pem"   
 
-const char* key_string = "f0edeccfa143a5b61e1998606f71b9710216ac7d8a1830b1236eed60e2747a43";
-const char* iv_string  = "edd98e3918bfab446883c42cce292e22";
-unsigned char g_key[32];
-unsigned char g_iv[AES_BLOCK_SIZE];
+
+enum Cmd{
+    CMD_OK      = 0,
+    CMD_NOK     = 1,
+    CMD_PRINT   = 2
+};
+
+typedef struct _msg{
+    unsigned int cmd;
+    char payload[BUFFER_SIZE-4];
+    size_t payload_len;
+}msg;
+
 
 void exit_sys(const char *msg);
 int hex_char_to_int(char c) ;
 int hexstr_to_bytes(const char *hexstr, unsigned char *out, size_t out_len);
-
-int add_padding(unsigned char*buf,size_t buf_len )
-{
-    int padding_len =  16 - (buf_len % 16 );
-    if(padding_len == 16)
-        return buf_len;
-    
-    memset(buf+buf_len, padding_len, padding_len);        
-
-    return buf_len+padding_len;
-}
-
-void print_hex(const char *label, const unsigned char *data, int len) 
-{
-    printf("%s: ", label);
-    for (int i = 0; i < len; i++) {
-        printf("%02x", data[i]);
-    }
-    printf("\n");
-}
-
-int createPacket(char* buf, size_t buf_size, char* data, int data_len, AES_KEY* enc_key)
-{   
-    int padded_len = add_padding(data,data_len);
-    int len=0;
-    char ciphertext[BUFFER_SIZE];
-
-    if(padded_len > (buf_size-(AES_BLOCK_SIZE+sizeof(len)) ))
-    {
-        return -1;
-    }
-
-    len = padded_len + AES_BLOCK_SIZE;
-
-    memcpy(buf, &len, sizeof(len));
-    memcpy(buf+sizeof(len), g_iv, AES_BLOCK_SIZE);
-
-    AES_cbc_encrypt(data, ciphertext, padded_len, enc_key, g_iv, AES_ENCRYPT);
-
-    memcpy(buf+sizeof(len)+AES_BLOCK_SIZE, ciphertext, padded_len);
-
-    return (padded_len+AES_BLOCK_SIZE+sizeof(len));
-}
+void print_hex(const char *label, const unsigned char *data, int len) ;
+int add_padding(unsigned char*buf,size_t buf_len );
+int create_packet(char* buf, int buf_size,const msg* message, unsigned char* iv, const AES_KEY* enc_key);
+int recv_all(int sock, void *buf, size_t len);
+int recv_server_msg(int client_sock, msg* message, AES_KEY* dec_key, unsigned char* iv );
+int unpad(unsigned char* buf, size_t size);
+void create_key(unsigned char* key, unsigned char* iv );
+int safe_delete_and_sync(const char *filepath);
+int recv_server_certificate(int client_sock);
 
 int main(void)
 {
@@ -76,13 +58,19 @@ int main(void)
     char *str;
     int string_len;
     AES_KEY enc_key;
+    AES_KEY dec_key;
     size_t recv_len;
     int result;
-            
-    hexstr_to_bytes(key_string,g_key, sizeof(g_key));
-    hexstr_to_bytes(iv_string, g_iv, sizeof(g_iv));
-    AES_set_encrypt_key(g_key, 256, &enc_key);
+    unsigned char key[32];
+    unsigned char iv[AES_BLOCK_SIZE];
+    msg message;
+
     
+
+    OpenSSL_add_all_algorithms();
+    OpenSSL_add_all_ciphers();
+    ERR_load_crypto_strings();
+
 
     if((client_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
         exit_sys("socket");
@@ -113,33 +101,73 @@ int main(void)
 	        exit_sys("recv");   
         
         recv_buf[recv_len] = '\0';
+        
         if(strcmp("HELLO", recv_buf) != 0)
-            goto EXIT;      
+        {
+            printf("Connection Failed\n");
+            goto EXIT;   
+        }
 
+        if(recv_server_certificate(client_sock) == -1)
+        {
+            exit_sys("Cannot Recv Server Certificate");
+        }
+
+        create_key(key, iv );
+        
+        AES_set_decrypt_key(key, 256, &dec_key);
+        AES_set_encrypt_key(key, 256, &enc_key);
+        
+        if( send_key(key,iv, client_sock )== -1)
+        {
+            exit_sys("Key exchange failed!!");
+        }
+
+        if(recv_server_msg( client_sock, &message, &dec_key, iv ) == -1)
+        {
+            exit_sys("Key exchange failed!!"); 
+        }
+
+        if(message.cmd != CMD_OK)
+        {
+            exit_sys("Key exchange failed!!");
+        }
 
         for (;;) 
         {
-		    printf("csd>");
+		    printf("ct>");
 		    fflush(stdout);
 
-		    if (fgets(buf, BUFFER_SIZE, stdin) == NULL)
+		    if (fgets(buf, BUFFER_SIZE-4, stdin) == NULL)
 			    continue;
 		    if ((str = strchr(buf, '\n')) != NULL)
 			    *str = '\0';
             
             string_len= strlen(buf);            
             
-            if( (result = createPacket(send_buf, BUFFER_SIZE, buf , string_len, &enc_key)) == -1)
+            message.cmd = CMD_PRINT;
+
+            memcpy(message.payload, buf, string_len);
+            message.payload_len = string_len;
+            if( (result = create_packet(send_buf, BUFFER_SIZE, &message, iv, &enc_key)) == -1)
             {
                 printf("Too Long Data \n");
                 continue;
             }
-
 		    if (send(client_sock, send_buf, result, MSG_NOSIGNAL) == -1)
 			    exit_sys("send"); 
 
-            buf[string_len] = '\0';
-            printf("%s \n", buf);  
+
+            if(recv_server_msg( client_sock, &message, &dec_key, iv ) == -1)
+            {
+                exit_sys("recv_server_msg");
+            }
+            
+            if(message.cmd != CMD_OK)
+            {
+                printf("Server Failed To Execute The Message !!\n");
+            }
+
             if (!strcmp(buf, "quit"))
 			    break;
 	    }
@@ -151,6 +179,148 @@ EXIT:
 	return 0;
     
 }
+
+int recv_server_certificate(int client_sock)
+{   
+   unsigned int message_len = 0;
+   FILE* fptr = 0;
+   char buf[BUFFER_SIZE];
+   size_t file_size = 0;
+   size_t rcv_len = 0;
+   size_t total_rcvd_bytes =0;
+
+
+    safe_delete_and_sync(SVR_KEY_PATH);
+    fptr = fopen(SVR_KEY_PATH, "wb");
+
+    if(fptr == NULL)
+        return -1;
+
+    if(recv_all(client_sock, &message_len, sizeof(message_len) ) == -1)
+    {
+        return -1;
+    }
+
+    if(message_len != sizeof(file_size))
+    {
+        return -1;
+    }
+
+    if(recv_all(client_sock, &file_size, message_len) == -1)
+    {
+        return -1;
+    }
+
+    for(;;)
+    {
+        if( (rcv_len = recv(client_sock, buf, 1024, 0)) == -1)
+        {
+            return -1;
+        }
+        fwrite(buf,sizeof(char),rcv_len, fptr);
+        total_rcvd_bytes += rcv_len;
+        if(total_rcvd_bytes >= file_size)
+        {
+            break;
+        }
+    }
+    fclose(fptr);
+    return 0;
+}
+
+
+void create_key(unsigned char* key, unsigned char* iv )
+{
+    RAND_bytes(key, 32);
+    RAND_bytes(iv, AES_BLOCK_SIZE);
+}
+
+int send_key(unsigned char* key, unsigned char* iv, int client_sock)
+{
+    // Load or generate RSA keypair
+    RSA *rsa = NULL;
+    // load private key from PEM file
+    FILE *fp = fopen(SVR_KEY_PATH, "r");
+    unsigned int message_len = 0;
+    int dec_len;
+    char payload[32+ AES_BLOCK_SIZE];
+    char buf[BUFFER_SIZE];
+    char encrypted[BUFFER_SIZE+1];
+    int enc_len = 0;
+    msg message;
+
+    if(fp == NULL)
+    {
+        return -1;
+    }
+    
+    if(PEM_read_RSA_PUBKEY(fp, &rsa, NULL, NULL) == NULL )
+    {
+        ERR_print_errors_fp(stderr);
+        fclose(fp);
+        return -1;
+    }
+    fclose(fp);
+
+    memcpy(payload, key, 32);
+    memcpy((payload + 32), iv, AES_BLOCK_SIZE);
+
+    enc_len = RSA_public_encrypt(48, payload, encrypted, rsa, RSA_PKCS1_OAEP_PADDING);
+
+    memcpy( buf, &enc_len, sizeof(enc_len) );
+    memcpy( buf+4, encrypted, enc_len );
+
+    if (send(client_sock, buf, enc_len+sizeof(enc_len), MSG_NOSIGNAL) == -1)
+    {
+	    return -1;
+    }
+    return 0;
+}
+
+int recv_server_msg(int client_sock, msg* message, AES_KEY* dec_key, unsigned char* iv )
+{
+    unsigned int message_len;
+    unsigned char buf[BUFFER_SIZE];
+    unsigned char decrypted[BUFFER_SIZE];
+    int dec_len = 0;
+
+    if(recv_all(client_sock, &message_len, sizeof(message_len) ) == -1)
+    {
+        return -1;
+    }
+
+    if(recv_all(client_sock, buf, message_len) == -1)
+    {
+        return -1;
+    }
+    memcpy(iv,buf, AES_BLOCK_SIZE);
+    
+    fflush(stdout);
+
+    AES_cbc_encrypt(buf+AES_BLOCK_SIZE, decrypted, (size_t)(message_len-AES_BLOCK_SIZE), dec_key, iv, AES_DECRYPT);
+    
+    fflush(stdout);
+    dec_len = unpad(decrypted, message_len-AES_BLOCK_SIZE) ;
+    
+
+    memcpy(&message->cmd, decrypted, 4);
+    memcpy(message->payload, decrypted, dec_len-4);
+    return 0;
+}
+
+int recv_all(int sock, void *buf, size_t len) 
+{
+    size_t total = 0;
+    while (total < len) 
+    {
+        ssize_t n = recv(sock, (char*)buf + total, len - total, 0);
+        if (n < 0) exit_sys("recv"); // error or disconnect
+        if (n == 0) return -1;
+        total += n;
+    }
+    return 0;
+}
+
 
 int hex_char_to_int(char c) 
 {
@@ -172,6 +342,110 @@ int hexstr_to_bytes(const char *hexstr, unsigned char *out, size_t out_len)
 
         out[i] = (high << 4) | low;
     }
+
+    return 0;
+}
+
+int add_padding(unsigned char*buf,size_t buf_len )
+{
+    int padding_len =  16 - (buf_len % 16 );
+    
+    memset(buf+buf_len, padding_len, padding_len);        
+
+    return buf_len+padding_len;
+}
+
+void print_hex(const char *label, const unsigned char *data, int len) 
+{
+    printf("%s: ", label);
+    for (int i = 0; i < len; i++) {
+        printf("%02x", data[i]);
+    }
+    printf("\n");
+}
+
+int create_packet(char* buf, int buf_size,const msg* message, unsigned char* iv, const AES_KEY* enc_key)
+{   
+    
+    int padded_len;
+    int len=0;
+    char ciphertext[BUFFER_SIZE];
+    char tmp_buf[BUFFER_SIZE];
+    size_t raw_msg_len;
+
+    memcpy(tmp_buf,&message->cmd, 4);
+    memcpy(tmp_buf+4, message->payload, message->payload_len );
+    
+    raw_msg_len = 4 + message->payload_len;
+        
+    padded_len = add_padding(tmp_buf,raw_msg_len);
+    
+    
+    if(padded_len > (buf_size-(AES_BLOCK_SIZE+sizeof(len)) ))
+    {
+        return -1;
+    }
+    len = padded_len + AES_BLOCK_SIZE;
+    memcpy(buf, &len, sizeof(len));
+    memcpy(buf+sizeof(len), iv, AES_BLOCK_SIZE);
+    AES_cbc_encrypt(tmp_buf, ciphertext, padded_len, enc_key, iv, AES_ENCRYPT);
+    // ToDo: Prevent Unnecessary Copy
+    memcpy(buf+sizeof(len)+AES_BLOCK_SIZE, ciphertext, padded_len);
+    // Return Total Length
+    return (padded_len+AES_BLOCK_SIZE+sizeof(len));
+}
+
+int unpad(unsigned char* buf, size_t size)
+{
+    int len_of_padding = buf[size-1];
+    if(len_of_padding <= 0)
+        return -1;
+    
+    return size-len_of_padding;
+}
+
+
+int safe_delete_and_sync(const char *filepath) 
+{
+    // Duplicate filepath because dirname() modifies it
+    char *dirpath;
+    int dirfd;
+    char buf[2048];
+
+
+    strcpy(buf,filepath);
+    buf[strlen(filepath)] = '\0';
+    // Get directory name
+    dirpath = dirname(buf);
+    if (!dirpath) 
+    {
+        perror("dirname");
+        return -1;
+    }
+
+    // Open directory
+    dirfd = open(dirpath, O_DIRECTORY | O_RDONLY);
+    if (dirfd == -1) 
+    {
+        perror("open directory");
+        return -1;printf("%s %d \n", __func__, __LINE__);
+    }
+
+    // Unlink (delete) the file
+    if (unlink(filepath) == -1) 
+    {
+        perror("unlink");
+        close(dirfd);
+        return -1;
+    }
+    // Fsync the directory
+    if (fsync(dirfd) == -1) 
+    {
+        perror("fsync");
+        close(dirfd);
+        return -1;
+    }
+    close(dirfd);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -7,17 +7,34 @@
 #include <arpa/inet.h>
 #include <openssl/aes.h>
 #include <openssl/rand.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
 #include <pthread.h>
 #include <stdatomic.h>
 
 #define SERVER_PORT 55555
 #define BUFFER_SIZE 4096
 #define MAX_CONNECTIONS 3
+#define PUB_CER_PATH    "./ServerFs/sample_public.pem"
+#define PRV_CER_PATH    "./ServerFs/sample_private.pem"
 
-const char* g_key_string = "f0edeccfa143a5b61e1998606f71b9710216ac7d8a1830b1236eed60e2747a43";
-const char* g_iv_string  = "edd98e3918bfab446883c42cce292e22";
-unsigned char g_key[32];
-AES_KEY g_dec_key;
+enum Cmd{
+    CMD_OK      = 0,
+    CMD_NOK     = 1,
+    CMD_PRINT   = 2
+};
+
+typedef struct tagCLIENT_INFO {
+	int sock;
+	struct sockaddr_in sin;
+} CLIENT_INFO;
+
+typedef struct _msg{
+    unsigned int cmd;
+    char payload[BUFFER_SIZE-4];
+    size_t payload_len;
+}msg;
 
 
 void *client_thread_proc(void *param);
@@ -25,15 +42,21 @@ void exit_sys(const char *msg);
 int hex_char_to_int(char c) ;
 int hexstr_to_bytes(const char *hexstr, unsigned char *out, size_t out_len);
 int unpad(unsigned char* buf, size_t size);
+int recv_key(unsigned char* key,unsigned char* iv, const CLIENT_INFO* ci );
+int recv_all(int sock, void *buf, size_t len);
 int connectionNum = 0;
+int create_packet(char* buf,const unsigned int buf_size,const msg* message, unsigned char *iv, const AES_KEY* enc_key);
+void print_hex(const char *label, const unsigned char *data, int len) ;
+int send_certificate(CLIENT_INFO* ci);
 
-
-typedef struct tagCLIENT_INFO {
-	int sock;
-	struct sockaddr_in sin;
-} CLIENT_INFO;
-
-
+void print_hex(const char *label, const unsigned char *data, int len) 
+{
+    printf("%s: ", label);
+    for (int i = 0; i < len; i++) {
+        printf("%02x", data[i]);
+    }
+    printf("\n");
+}
 
 int main(void)
 {
@@ -43,10 +66,10 @@ int main(void)
 	pthread_t tid;
 	CLIENT_INFO *ci;
 	int result;
-    
-    hexstr_to_bytes(g_key_string,g_key, sizeof(g_key));
-    AES_set_decrypt_key(g_key, 256, &g_dec_key);
 
+    OpenSSL_add_all_algorithms();
+    OpenSSL_add_all_ciphers();
+    ERR_load_crypto_strings();
 
     if((server_sock= socket(AF_INET, SOCK_STREAM,0)) == -1)
         exit_sys("socket");
@@ -71,7 +94,6 @@ int main(void)
         client_sock = accept(server_sock, (struct sockaddr*)&sin_client, &sin_len);
         if( client_sock == -1 )
         {
-            printf("%d \n", client_sock);
             exit_sys("accept");            
 			//exit_sys("send");
         }
@@ -86,7 +108,7 @@ int main(void)
             close(client_sock);
             continue;
         }
-        send(client_sock, "HELLO", 5, 0);
+        send(client_sock, "HELLO", 5, MSG_NOSIGNAL);
         connectionNum++;
         printf("%d\n", connectionNum);
         
@@ -115,16 +137,14 @@ int main(void)
 	}    
 
     shutdown(client_sock, SHUT_RDWR);
-    close(client_sock);
-
     close(server_sock);  
-
 }
 
 void *client_thread_proc(void *param)
 {
-	char buf[BUFFER_SIZE+1];
+	char buf[BUFFER_SIZE];
     char decrypted[BUFFER_SIZE+1];
+    char send_buf[BUFFER_SIZE];
 	char ntopbuf[INET_ADDRSTRLEN];
 	unsigned port;
 	ssize_t result;
@@ -132,9 +152,33 @@ void *client_thread_proc(void *param)
     int message_len;
     ssize_t actual_len;
     unsigned char iv[16];
+    unsigned char key[32];
+    AES_KEY dec_key;
+    AES_KEY enc_key;
+    msg message;
+    msg msg_to_send;
+    int send_len;
 
 	inet_ntop(AF_INET, &ci->sin.sin_addr, ntopbuf, INET_ADDRSTRLEN);
 	port = (unsigned)ntohs(ci->sin.sin_port);
+
+    if(send_certificate(ci) == -1)
+        exit_sys("Cannot send certificate!!");
+
+
+    if(recv_key(key,iv, ci ) == -1)
+        exit_sys("Key Exchange Failed!!");    
+
+
+    AES_set_decrypt_key(key, 256, &dec_key);
+    AES_set_encrypt_key(key, 256, &enc_key);
+    
+    msg_to_send.cmd = CMD_OK;
+    msg_to_send.payload_len = 0;
+    
+
+    send_len = create_packet(send_buf, BUFFER_SIZE, &msg_to_send, iv, &enc_key);
+    send(ci->sock,send_buf,send_len,MSG_NOSIGNAL);
 
 	for (;;) 
     {   
@@ -142,27 +186,43 @@ void *client_thread_proc(void *param)
         {
             break;   
         }
-
         if(recv_all(ci->sock, buf, message_len) == -1)
         {
             break;
         }
         memcpy(iv,buf, AES_BLOCK_SIZE);
-        
-        AES_cbc_encrypt(&buf[AES_BLOCK_SIZE], decrypted, message_len-AES_BLOCK_SIZE, &g_dec_key, iv, AES_DECRYPT);
-        
+        AES_cbc_encrypt(&buf[AES_BLOCK_SIZE], decrypted, message_len-AES_BLOCK_SIZE, &dec_key, iv, AES_DECRYPT);
         actual_len = unpad(decrypted,message_len-AES_BLOCK_SIZE);
         
         if(-1 == actual_len)
             exit_sys("unpad");		
 
-        decrypted[actual_len] = '\0';
-		if (!strcmp(decrypted, "quit"))
-			break;
-		printf("%ld byte(s) received: \"%s\"\n", actual_len, decrypted);
-        
-		//if (send(ci->sock, buf, result, 0) 1== -1)
-			//exit_sys("send");
+        memcpy(&message.cmd,decrypted,4);
+        memcpy(message.payload, decrypted+4, (actual_len-4));
+        message.payload_len = actual_len-4;
+
+        if(message.cmd == CMD_PRINT)
+        {
+            msg_to_send.cmd = CMD_OK;
+            msg_to_send.payload_len = 0;
+            
+            send_len = create_packet(send_buf, BUFFER_SIZE, &msg_to_send, iv, &enc_key);
+            send(ci->sock,send_buf,send_len,MSG_NOSIGNAL);
+            message.payload[message.payload_len] = '\0';
+            printf("%s \n", message.payload);
+            
+            if (!strcmp(message.payload, "quit"))
+			    break;
+        }
+        else
+        {
+            msg_to_send.cmd = CMD_NOK;
+            msg_to_send.payload_len = 0;
+            
+            send_len = create_packet(send_buf, BUFFER_SIZE, &msg_to_send, iv, &enc_key);
+            send(ci->sock,send_buf,send_len,MSG_NOSIGNAL);
+            printf("Unknown Command Received \n");
+        }
 	}
 
 	printf("client disconnected %s:%u\n", ntopbuf, port);
@@ -177,6 +237,125 @@ void *client_thread_proc(void *param)
 	return NULL;
 }
 
+int send_certificate(CLIENT_INFO* ci)
+{
+    FILE* fptr;
+    size_t f_size = 0;
+    char buf[BUFFER_SIZE];
+    unsigned int size_of_size_t = (int)sizeof(f_size);
+    size_t read_len = 0;
+    size_t total_len = 0;
+
+    fptr = fopen(PUB_CER_PATH,"rb");
+    if(fptr ==NULL)
+    {
+        return -1;
+    }
+    fseek(fptr, 0, SEEK_END);
+    f_size = ftell(fptr);
+    if(f_size == -1)
+    {
+        return -1;
+    }
+    fseek(fptr, 0, SEEK_SET);
+    // packet = LEN || FILE_LEN
+    memcpy(buf,&size_of_size_t,sizeof(size_of_size_t));
+    memcpy(&buf[sizeof(size_of_size_t)],&f_size, size_of_size_t);
+    
+    if(send(ci->sock, buf, (sizeof(size_of_size_t) + size_of_size_t), MSG_NOSIGNAL) == -1)
+    {
+        return -1;
+    }
+
+    for(;;)
+    {
+        read_len = fread(buf,sizeof(char),1024,fptr );
+        if(send(ci->sock, buf, read_len, MSG_NOSIGNAL) == -1)
+        {
+            return -1;
+        }
+        
+        total_len += read_len;
+        if(total_len == f_size)
+            break;
+    }
+    fclose(fptr);
+    return 0;
+}
+
+
+int add_padding(unsigned char*buf,const size_t buf_len )
+{
+    int padding_len =  16 - (buf_len % 16 );
+    /*if(padding_len == 16)
+        return buf_len;*/
+    
+    memset(buf+buf_len, padding_len, padding_len);        
+
+    return buf_len+padding_len;
+}
+
+int create_packet(char* buf, const unsigned int buf_size,const msg* message, unsigned char *iv, const AES_KEY* enc_key)
+{   
+    int padded_len;
+    int len=0;
+    char ciphertext[BUFFER_SIZE];
+    char tmpBuf[BUFFER_SIZE];
+    size_t tmp_buf_size;
+    
+    memcpy(tmpBuf,&message->cmd, 4);
+    memcpy(tmpBuf+4, message->payload, message->payload_len );
+    tmp_buf_size= 4+ message->payload_len;
+    
+    padded_len = add_padding(tmpBuf,tmp_buf_size);
+    
+    if(padded_len > (buf_size-(AES_BLOCK_SIZE+sizeof(len)) ))
+    {
+        return -1;
+    }
+    len = padded_len + AES_BLOCK_SIZE;
+    memcpy(buf, &len, sizeof(len));
+    memcpy(buf+sizeof(len), iv, AES_BLOCK_SIZE);
+    AES_cbc_encrypt(tmpBuf, ciphertext, padded_len, enc_key, iv, AES_ENCRYPT);
+    // ToDo: Prevent Unnecessary Copy
+    memcpy(buf+sizeof(len)+AES_BLOCK_SIZE, ciphertext, padded_len);
+
+    return (padded_len+AES_BLOCK_SIZE+sizeof(len));
+}
+
+int recv_key(unsigned char* key,unsigned char* iv, const CLIENT_INFO* ci )
+{
+    // Load or generate RSA keypair
+    RSA *rsa = NULL;
+    // load private key from PEM file
+    FILE *fp = fopen(PRV_CER_PATH, "r");
+    unsigned int message_len = 0;
+    int dec_len;
+    char buf[BUFFER_SIZE];
+    char decrypted[BUFFER_SIZE+1];
+
+    if(PEM_read_RSAPrivateKey(fp, &rsa, NULL, NULL) == NULL )
+    {
+        ERR_print_errors_fp(stderr);
+        fclose(fp);
+        return -1;
+    }
+    fclose(fp);
+    if(recv_all(ci->sock, &message_len, sizeof(message_len) ) == -1)
+    {
+        return -1;
+    }
+    if(recv_all(ci->sock, buf, message_len) == -1)
+    {
+        return -1;
+    }
+    dec_len = RSA_private_decrypt(message_len, buf, decrypted, rsa, RSA_PKCS1_OAEP_PADDING );
+
+    memcpy(key, decrypted, 32);
+    memcpy(iv, decrypted + 32, 16);
+    return 0;
+}
+
 int recv_all(int sock, void *buf, size_t len) 
 {
     size_t total = 0;
@@ -189,14 +368,25 @@ int recv_all(int sock, void *buf, size_t len)
     }
     return 0;
 }
-
 int unpad(unsigned char* buf, size_t size)
+
 {
-    int len_of_padding = buf[size-1];
-    if(len_of_padding <= 0)
-        return -1;
-    
-    return size-len_of_padding;
+    unsigned char padding_value = buf[size - 1];
+
+    // Padding must be between 1 and 16
+    if (padding_value < 1 || padding_value > 16) {
+        return 0; // No padding detected
+    }
+
+    // Check that all padding bytes are correct
+    for (int i = 0; i < padding_value; i++) {
+        if (buf[size - 1 - i] != padding_value) {
+            return 0; // No padding detected
+        }
+    }
+
+    return size - padding_value;
+
 }
 
 
